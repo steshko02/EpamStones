34e950ea3699107c0992f0ebe7d7d4db77b849db
steshko02 <steshko02@mail.ru>
2021-03-18 20:47:51 +0300

    All fix test. Have not good logging

--- src/test/java/entity/NecklaceTest.java
+++ src/test/java/entity/NecklaceTest.java
@@ -6,72 +6,57 @@ import characteristic.PreciousStoneType;
 import characteristic.SemiPreciousStoneType;
 import characteristic.Transparency;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 public class NecklaceTest {
     static  List<Stone> underTest = new ArrayList<>();
+
     @BeforeClass
     public static void init(){
         underTest.add(new PreciousStone(Color.Red, Transparency.Opaque, 1000f,20, PreciousStoneType.Ruby));
         underTest.add(new SemiPreciousStone(Color.Red,Transparency.SemiTranslucent,333f,6, SemiPreciousStoneType.Epidote));
         underTest.add(new Stone(Color.Red, Transparency.Opaque, 1001f));
-
     }
+
     @Test
     public void fullCost() {
 
-//        List<Stone> list = new ArrayList<>();
-//        list.add(new PreciousStone(Color.Red, Transparency.Opaque, 1000f,20, PreciousStoneType.Ruby));
-//        list.add(new SemiPreciousStone(Color.Red,Transparency.SemiTranslucent,333f,6, SemiPreciousStoneType.Epidote));
-//     //   expected.add(new Stone(Color.Red, Transparency.Opaque, 1000f));
         Necklace necklace = new Necklace(underTest);
         float actual=1000f+1001f+333f;
         float expected=necklace.fullCost();
         Assert.assertEquals(expected,actual, 0.0f);
     }
+
     @Test(expected = ClassCastException.class)
     public void fullWeightClassCastException() {
-
-//        List<Stone> list = new ArrayList<>();
-//        list.add(new PreciousStone(Color.Red, Transparency.Opaque, 1000f,20, PreciousStoneType.Ruby));
-//        list.add(new SemiPreciousStone(Color.Red,Transparency.SemiTranslucent,333f,6, SemiPreciousStoneType.Epidote));
-//        list.add(new Stone(Color.Red, Transparency.Opaque, 1000f));
         Necklace necklace = new Necklace(underTest);
         necklace.fullWeight();
     }
+
     @Test
     public void SortByCost() {
-
-//        Stone stone1=new PreciousStone(Color.Red, Transparency.Opaque, 1000f,20, PreciousStoneType.Ruby);
-//        Stone stone2=new SemiPreciousStone(Color.Red,Transparency.SemiTranslucent,333f,6, SemiPreciousStoneType.Epidote);
-//        Stone stone3=new Stone(Color.Red, Transparency.Opaque, 1001f);
-//
-//        List<Stone> expected = new ArrayList<>();
-//        expected.add(stone1);
-//        expected.add(stone2);
-//        expected.add(stone3);
-
         SortByCost.stonesSortByCost(underTest);
 
         List<Stone> actual = new ArrayList<>();
+
         actual.add(new SemiPreciousStone(Color.Red,Transparency.SemiTranslucent,333f,6, SemiPreciousStoneType.Epidote));
         actual.add(new PreciousStone(Color.Red, Transparency.Opaque, 1000f,20, PreciousStoneType.Ruby));
         actual.add(new Stone(Color.Red, Transparency.Opaque, 1001f));
 
         Assert.assertEquals(underTest,actual);
     }
+
     @Test(expected = NullPointerException.class)
     public void fullWeightNullPointerException() {
         Necklace expected = new Necklace(null);
+
         expected.fullWeight();
     }
+
     @Test(expected = NullPointerException.class)
     public void fullCostNullPointerException() {
         Necklace expected = new Necklace(null);
